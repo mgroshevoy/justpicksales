75c1c689b137760627c20db455d00c1175c5a2e0
Maksim Groshevoi <mgroshevoy@gmail.com>
2016-12-14 20:28:57 +0100

    Add cache to response

--- routes/searching.js
+++ routes/searching.js
@@ -10,7 +10,7 @@ const axiosRetry = require('axios-retry');
 
 let flag = false;
 //let arrayOrders = [];
-//let orders = [];
+let orders;
 
 /**
  * Ebay API request to Trading:GetOrders
@@ -34,7 +34,7 @@ function ebayRequest(intPage = 1) {
 
             params: {
                 OrderStatus: 'All',
-                CreateTimeFrom: moment().subtract(30, 'days').toISOString(),
+                CreateTimeFrom: moment().subtract(90, 'days').toISOString(),
                 CreateTimeTo: moment().toISOString(),
                 OrderRole: 'Seller',
                 Pagination: {
@@ -88,169 +88,61 @@ function getOrdersFromEbay() {
             return res;
         }).catch(error => {
             console.error(error);
+            reject(error);
         }).then(results => {
             let promises = [];
             _.forEach(results, function (o, i) {
                 _.forEach(o.Items, function (item, j) {
-                    promises.push(new Promise((resolve) => {
-                        axiosRetry(axios, {retries: 3});
-                        axios.get("http://cgi.ebay.com/ws/eBayISAPI.dll?ViewItem&item=" + item.ItemID)
-                            .then((response) => {
-                                $ = cheerio.load(response.data);
-                                results[i].Items[j].Image = $('#icImg')['0'].attribs.src;
-                                resolve(results);
+                    promises.push(new Promise((resolve, reject) => {
+                        //                        axiosRetry(axios, {retries: 3});
+                        setTimeout(function () {
+                            axios.get("http://cgi.ebay.com/ws/eBayISAPI.dll?ViewItem&item=" + item.ItemID)
+                                .then((response) => {
+                                    $ = cheerio.load(response.data);
+                                    results[i].Items[j].Image = $('#icImg')['0'].attribs.src;
+                                    resolve(results);
+                                }).catch(error => {
+                                console.error(error);
+                                reject(error);
                             });
+                        }, i * 200);
                     }));
                 })
             });
             return Promise.all(promises);
+        }).catch(error => {
+            console.error(error);
+            reject(error);
         }).then(results => {
-            console.log(results);
-            console.log('Done!');
-            results = _.orderBy(results, ['PaidTime']);
-            resolve(results[results.length-1]);
+           // console.log(results);
+            console.log('Search is done!');
+            results = _.orderBy(results, ['PaidTime'], ['desc']);
+            resolve(results[results.length - 1]);
         }).catch(error => {
             console.error(error);
             reject(error);
         });
     });
-
-    //    orders = [];
-//    arrayOrders = [];
-    // return new Promise((resolve, reject) => {
-    //     ebay.xmlRequest({
-    //         serviceName: 'Trading',
-    //         opType: 'GetOrders',
-    //
-    //         // app/environment
-    //         devId: process.env.EBAY_DEVID,
-    //         certId: process.env.EBAY_CERTID,
-    //         appId: process.env.EBAY_APPID,
-    //         sandbox: false,
-    //
-    //         // per user
-    //         authToken: process.env.EBAY_AUTHTOKEN,
-    //
-    //         params: {
-    //             OrderStatus: 'All',
-    //             CreateTimeFrom: moment().subtract(30, 'days').toISOString(),
-    //             CreateTimeTo: moment().toISOString(),
-    //             OrderRole: 'Seller',
-    //             Pagination: {
-    //                 EntriesPerPage: 100,
-    //                 PageNumber: 1
-    //             }
-    //         }
-    //     }, function (error, results) {
-    //         let i, prom = [];
-    //         if (error) {
-    //             console.error(error);
-    //             reject(error);
-    //         }
-    //         arrayOrders.push(results);
-    //         for (i = 2; i <= results.PaginationResult.TotalNumberOfPages; i++) {
-    //             prom.push(new Promise((resolve, reject) => {
-    //                 ebay.xmlRequest({
-    //                     serviceName: 'Trading',
-    //                     opType: 'GetOrders',
-    //
-    //                     // app/environment
-    //                     devId: process.env.EBAY_DEVID,
-    //                     certId: process.env.EBAY_CERTID,
-    //                     appId: process.env.EBAY_APPID,
-    //                     sandbox: false,
-    //
-    //                     // per user
-    //                     authToken: process.env.EBAY_AUTHTOKEN,
-    //
-    //                     params: {
-    //                         OrderStatus: 'All',
-    //                         CreateTimeFrom: moment().subtract(30, 'days').toISOString(),
-    //                         CreateTimeTo: moment().toISOString(),
-    //                         OrderRole: 'Seller',
-    //                         Pagination: {
-    //                             EntriesPerPage: 100,
-    //                             PageNumber: i
-    //                         }
-    //                     }
-    //                 }, function (error, results) {
-    //                     if (error) {
-    //                         console.log(error);
-    //                         reject(error);
-    //                     }
-    //                     arrayOrders.push(results);
-    //                     resolve(results);
-    //                 });
-    //             }));
-    //         }
-    //         Promise.all(prom).then(function () {
-    //             if (arrayOrders[0].Orders) {
-    //                 _.forEach(arrayOrders, function (o) {
-    //                     _.forEach(o.Orders, function (order) {
-    //                         orders.push({
-    //                             OrderID: order.OrderID,
-    //                             OrderStatus: order.OrderStatus,
-    //                             Items: [],
-    //                             eBayPaymentStatus: order.CheckoutStatus.eBayPaymentStatus,
-    //                             Status: order.CheckoutStatus.Status,
-    //                             PaidTime: order.PaidTime,
-    //                             Total: order.Total._
-    //                         });
-    //                         _.forEach(order.Transactions, function (transaction) {
-    //                             orders[orders.length - 1].Items.push({
-    //                                 ItemID: transaction.Item.ItemID,
-    //                                 Title: transaction.Item.Title,
-    //                                 SKU: transaction.Item.SKU,
-    //                             });
-    //                         })
-    //                     })
-    //                 });
-    //             }
-    //         }).then(function () {
-    //             let prom = [];
-    //
-    //             _.forEach(orders, function (o, i) {
-    //                 _.forEach(o.Items, function (item, j) {
-    //                     prom.push(new Promise((resolve, reject) => {
-    //                         axiosRetry(axios, {retries: 3});
-    //                         axios.get("http://cgi.ebay.com/ws/eBayISAPI.dll?ViewItem&item=" + item.ItemID)
-    //                             .then((response) => {
-    //                                 $ = cheerio.load(response.data);
-    //                                 orders[i].Items[j].Image = $('#icImg')['0'].attribs.src;
-    //                                 resolve(orders[i].Items[j].Image);
-    //                                 // }).catch((error) => {
-    //                                 // console.error(error);
-    //                                 // reject(error);
-    //                             });
-    //                     }));
-    //                 })
-    //             });
-    //             return Promise.all(prom);
-    //         }).then(function () {
-    //             console.log(orders);
-    //             console.log('Done!');
-    //             orders = _.orderBy(orders, ['PaidTime']);
-    //             resolve(orders);
-    //         }).catch((error) => {
-    //             console.error(error);
-    //             reject(error);
-    //         });
-    //     });
-    // });
 }
 
 router.get('/', function (req, res, next) {
     console.log('search ' + flag);
-    if (!flag) {
+    if (!flag && !(_.isArray(orders))) {
         flag = true;
         getOrdersFromEbay().then(results => {
             console.log('Work is done!');
             res.send({orders: results});
-            flag = false;
+            orders = _.clone(results);
+            console.log(orders);
+//            flag = false;
         }).catch(error => {
             console.error(error);
         });
     } else {
+        if (_.isArray(orders)) {
+            res.send({orders: orders});
+        } else res.send({inProgress: true});
+        console.log(orders);
         //res.render('orders', {title: 'Orders', orders: orders});
         //res.send();
     }
